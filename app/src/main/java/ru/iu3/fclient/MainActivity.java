fea69ff71c270dda1c5edd1788cf5ba021f576cd
Ann-blnv <blinova.anna.2000@gmail.com>
2021-03-24 15:27:38 +0300

    initial commit

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -2,10 +2,19 @@ package ru.iu3.fclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-import java.nio.charset.StandardCharsets;
+import android.widget.Toast;
 
+import java.nio.charset.StandardCharsets;
+//import org.appache.commons.codec.DecoderException;
+//import org.appache.commons.codec.binary.Hex;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -21,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = findViewById(R.id.sample_text);
+       // tv.setText(stringFromJNI());
 
         byte[] key = randomBytes(16);
         byte[] data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
@@ -45,6 +54,53 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Original: "  + originalData);
         System.out.println("Encrypted: " + encryptedData);
         System.out.println("Decrypted: " + decryptedData);
+    }
+    //вспомогательная функция для преобразования строки в двоичный массив.
+    public static byte[] StringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+    public void onButtonClick(View v){
+        //Добавим тест шифрации и дешифрации TDEA. Для наглядности зададим
+        //ключ и данные HEX-ASCII строками и воспользуемся процедурами преобразования
+        //в массив байтов и обратно из библиотеки commons-codec.
+        //Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+        byte [] key = StringToHex("0123456789ABCDEF0123456789ABCDE0");
+        byte [] enc = encrypt(key,StringToHex("000000000000000102"));
+        byte [] dec = decrypt(key,enc);
+        String s = new String (Hex.encodeHex(dec)).toUpperCase();
+        //Воспользуемся для вывода сообщений классом Toast
+        //Этот код выводит на экран сообщение “Clicked”, которое исчезает через некоторое время.
+       //Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        // В обработчике onButtonClick заменим код на вызов PinpadActivity.
+       // Intent it = new Intent(this, PinpadActivity.class);
+       // startActivity(it);
+        //Заменим в MainAcitivity вызов PinpadActivity на
+        Intent it = new Intent(this, PinpadActivity.class);
+        startActivityForResult(it, 0);
+        //Теперь видно откуда берется requestCode.
+        // Это значение нужно для того чтобы идентифицировать ответы от разных вызовов Activity.
+    }
+    //Добавим к классу MainActivity обработчик результата.
+    //Эта функция будет вызвана, когда PinpadActivity отправит нам Intent с результатом.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK || data != null) {
+                String pin = data.getStringExtra("pin");
+                Toast.makeText(this, pin, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     /**
